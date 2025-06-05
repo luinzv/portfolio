7427273918393a817484ae5a3e73db02e15d5198
Luis <luis.contreras.dev.02@gmail.com>
2025-06-05 00:59:56 -0400

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import './App.css'
+import { downloadIcon, linkedinIcon, githubIcon, reactIcon, typescriptIcon, postgresqlIcon, dockerIcon, CSS3Icon, javaIcon, AWSIcon, microservicesIcon, gitIcon, menuIcon,} from '../src/assets/img/icons'
+function App() {
+
+  return (
+    <>
+      <div id="app-root">
+        <div id="content-wrapper">
+          <nav className='navbar-menu'>
+            <div className="container-menu">
+              <img src={menuIcon} alt="" className='menu-icon'/>
+            </div>
+          </nav>
+          <section className='home'>
+            <header className='header-container'>
+              <a href="#" className='download'>
+                <img src={downloadIcon} alt="svg-download" className='svg-download' />
+                <span>Descargar CV</span>
+              </a>
+              <h1> Software Enginner</h1>
+              <h2>Luis Contreras</h2>
+              <h3>Full Stack React & Microservices</h3>
+              <div className="ancor-container">
+                <a href="#">
+                  Linkedin
+                  <img src={linkedinIcon} alt="linkedin-svg" className='socialIcons' />
+                </a>
+                <a href="#">
+                  Github
+                  <img src={githubIcon} alt="github-svg" className='socialIcons' />
+                </a>
+              </div>
+              <footer className='tech-stack'>
+                <img src={reactIcon} alt="react" className='tech-icons' />
+                <img src={typescriptIcon} alt="ts-svg" className='tech-icons'/>anizer
+                <img src={AWSIcon} alt="AWS-svg" className='tech-icons'/>
+                <img src={dockerIcon} alt="docker-svg" className='tech-icons'/>
+                <img src={CSS3Icon} alt="CSS3-svg" className='tech-icons'/>
+                <img src={javaIcon} alt="java-svg" className='tech-icons'/>
+                <img src={postgresqlIcon} alt="postgreSQL-svg" className='tech-icons'/>
+                <img src={microservicesIcon} alt="microservices-svg" className='tech-icons'/>
+                <img src={gitIcon} alt="git-svg" className='tech-icons'/>
+              </footer>
+            </header>
+          </section>
+
+          <section className='featured projects'>
+            <div className="project-container">
+
+            </div>
+          </section>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/img/icons.tsx
@@ -0,0 +1,19 @@
+import downloadIcon from './download.svg'
+import linkedinIcon from './linkedin.svg'
+import githubIcon from './github.svg'
+import reactIcon from './react.svg'
+import typescriptIcon from './typescript.svg'
+import postgresqlIcon from './postgresql.svg'
+import redisIcon from './redis.svg'
+import dockerIcon from './docker.svg'
+import CSS3Icon from './cssc.svg'
+import javaIcon from './java.svg'
+import AWSIcon from './aws.svg'
+import cybersegurityIcon from './cibersegurity.svg'
+import microservicesIcon from './microservices.svg'
+import gitIcon from './git.svg'
+import menuIcon from './menu.svg'
+
+export { downloadIcon, linkedinIcon, githubIcon, reactIcon, typescriptIcon, postgresqlIcon, redisIcon, dockerIcon, CSS3Icon,
+  javaIcon, AWSIcon,cybersegurityIcon, microservicesIcon, gitIcon, menuIcon
+}
